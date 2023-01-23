dec7bf828b4ce429b70356e44a8375798efbc69d
smvd <semvd06@gmail.com>
2023-01-23 09:19:44 +0100

    Change over to new site

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
-function OpenMenu() {
-    document.getElementById("menu").style.height = "100%";
-}
+const topButtonTrigger = 100;
+const topButton = document.getElementById('topButton')
+const header = document.getElementById('header')
 
-function CloseMenu() {
-    document.getElementById("menu").style.height = "0%";
+const scrollContainer = () => {
+  return document.documentElement || document.body;
+};
+
+document.addEventListener("scroll", () => {
+  if (scrollContainer().scrollTop > topButtonTrigger) {
+    topButton.classList.remove('fade');
+    header.classList.add('slide');
+  } else {
+    topButton.classList.add('fade');
+    header.classList.remove('slide');
+  }
+})
+
+function UpdateMenu() {
+    let items = document.getElementsByClassName("menuIcon")
+    for (let i = 0; i < items.length; i++) {
+        items[i].classList.toggle("change");
+    }
+
+    if (document.getElementById("menuOverlay").style.height == "100vh") {
+        document.getElementById("menuOverlay").style.height = "0vh";
+    } else {
+        document.getElementById("menuOverlay").style.height = "100vh";
+    }
 }
